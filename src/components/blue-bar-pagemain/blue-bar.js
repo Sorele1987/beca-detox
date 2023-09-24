58da9cfe2b199e51bf277fa672a2895fb814b4a8
Noppo <sorele.ma@gmail.com>
2023-09-24 15:15:40 -0400

    Se ha creado el repositio de Beca-detox

---
+++ src/components/blue-bar-pagemain/blue-bar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="blue_bar_container">
+      <h3>90 DAYS MONEY BACK GUARANTEE</h3>
+    </div>
+  );
+}
